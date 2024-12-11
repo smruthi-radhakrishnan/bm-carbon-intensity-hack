6789f2c47672f46cfcfb83f42599044ba589e3c8
smruthi-r <smruthi.radhakrishnan@kaluza.com>
2024-12-11 11:55:41 +0000

    Add emissions graph

---
+++ run.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from app.components.graph_plotting_functions import plot_emissions_over_time
+
+
+def run_app():
+    st.title("Group 7 Demo: Green Dispatch")
+    emissions_per_sp_df = pd.read_csv("data/test_results.csv")
+    line_plot = plot_emissions_over_time(emissions_per_sp_df)
+    st.plotly_chart(line_plot)
+    
+
+if __name__ == "__main__":
+    run_app()
