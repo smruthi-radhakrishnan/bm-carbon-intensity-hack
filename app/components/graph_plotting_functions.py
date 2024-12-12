b5e01cb8af523a63f2cfaadd779cb9bb98b08693
oksanazimina1 <166135048+oksanazimina1@users.noreply.github.com>
2024-12-12 10:11:10 +0000

    table

--- app/components/graph_plotting_functions.py
+++ app/components/graph_plotting_functions.py
@@ -1,23 +1,73 @@
 import pandas as pd
 import plotly.express as px
+import streamlit as st
 
 
 # SP = Settling Period
-def plot_emissions_over_time(output_df: pd.DataFrame):
+def plot_emissions_over_time(
+    output_df: pd.DataFrame,
+    axis_min=None,
+    axis_max=None,
+    title="Baseline CO₂ emissions over time (tonnes)",
+    y_label="CO₂ (tonnes)",
+    x_label="Settlement period start time (GMT)",
+    line_colour="#FF0000",
+):
     emissions_per_sp = output_df.groupby("start_time_gmt", as_index=False)[
         "carbon_emissions_tonnes"
     ].sum()
     fig = px.line(emissions_per_sp, x="start_time_gmt", y="carbon_emissions_tonnes")
-    fig.update_layout(title="Emissions over time, tonnes")
+    fig.update_layout(title=title)
+    fig.update_xaxes(title=x_label)
+    fig.update_yaxes(title=y_label)
+    if axis_max:
+        fig.update_yaxes(range=(axis_min, axis_max))
     return fig
 
 
-def plot_cost_over_time(output_df: pd.DataFrame):
+def plot_cost_over_time(
+    output_df: pd.DataFrame,
+    axis_min=None,
+    axis_max=None,
+    title="Baseline cost over time (£)",
+    y_label="Cost (£)",
+    x_label="Settlement period start time (GMT)",
+    line_colour="#324AB2",
+):
     cost_per_sp = output_df.groupby("start_time_gmt", as_index=False)[
         "total_cost_pounds"
     ].sum()
     fig = px.line(cost_per_sp, x="start_time_gmt", y="total_cost_pounds")
-    fig.update_layout(title="Cost over time, £")
-    fig.update_traces(line=dict(color="#FF0000"), mode="lines")
+    fig.update_layout(title=title)
+    fig.update_xaxes(title=x_label)
+    fig.update_yaxes(title=y_label)
+    if axis_max:
+        fig.update_yaxes(range=(axis_min, axis_max))
+    fig.update_traces(line=dict(color=line_colour), mode="lines")
 
     return fig
+
+
+def summary_table(original_df=None, adjusted_df=None):
+
+    original_cost = (
+        str((original_df["total_cost_pounds"].sum() / 1000000).round(1)) + "M"
+    )
+    adjusted_cost = (
+        str((adjusted_df["total_cost_pounds"].sum().round(2) / 1000000).round(1)) + "M"
+    )
+
+    original_carbon = str(original_df["carbon_emissions_tonnes"].sum())
+    adjusted_carbon = str(adjusted_df["carbon_emissions_tonnes"].sum())
+
+    data = {
+        "": ["Cost (£)", "CO₂ (tonnes)"],
+        "Baseline": [original_cost, original_carbon],
+        "Emissions-adjusted": [adjusted_cost, adjusted_carbon],
+    }
+    df = pd.DataFrame(data=data)
+    df = df.reset_index(drop=True)
+
+    return st.table(
+        data=df,
+    )
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
+import datetime
 from app.components.graph_plotting_functions import (
     plot_emissions_over_time,
     plot_cost_over_time,
+    summary_table,
 )
 from bmu_ranking import BmuRanking
 from ranking_methods import RankingMethod
@@ -18,21 +20,75 @@ def run_app():
     co_cost = 0.0
 
     all_boas_df = pd.read_parquet("data/carbon_emissions_results.parquet")
-    bmu_ranking_class = BmuRanking(all_boas_df, RankingMethod(ranking_mode), co_cost)
+    bmu_ranking_class_original = BmuRanking(
+        all_boas_df, RankingMethod(ranking_mode), 0.0
+    )
+    bmu_ranking_class_adjusted = BmuRanking(
+        all_boas_df, RankingMethod(ranking_mode), co_cost
+    )
 
-    cost_ranked_df = bmu_ranking_class.ranked_bmu_df
+    adjusted_costs_df = bmu_ranking_class_adjusted.ranked_bmu_df
+    original_costs_df = bmu_ranking_class_original.ranked_bmu_df
 
-    cost_over_time_plot = plot_cost_over_time(cost_ranked_df)
-    st.plotly_chart(cost_over_time_plot)
+    summary_table(original_df=original_costs_df, adjusted_df=adjusted_costs_df)
+
+    # Cost graphs
+
+    cost_max = (
+        max(
+            adjusted_costs_df["total_cost_pounds"].max(),
+            original_costs_df["total_cost_pounds"].max(),
+        )
+        + 100000
+    )
+    cost_min = (
+        min(
+            adjusted_costs_df["total_cost_pounds"].min(),
+            original_costs_df["total_cost_pounds"].min(),
+        )
+        - 100000
+    )
+
+    cost_over_time_plot = plot_cost_over_time(
+        original_costs_df,
+        axis_min=cost_min,
+        axis_max=cost_max,
+    )
+    col1, col2 = st.columns(2)
+
+    cost_over_time_plot_adjusted = plot_cost_over_time(
+        adjusted_costs_df,
+        title="Emissions-adjusted cost over time (£)",
+        line_colour="#000080",
+        axis_min=cost_min,
+        axis_max=cost_max,
+    )
+    col1.plotly_chart(cost_over_time_plot)
+    col2.plotly_chart(cost_over_time_plot_adjusted)
+
+    # date_selector = st.slider(
+    #     "Date selector",
+    #     min_value="2023-01-01",
+    #     max_value="2024-12-31",
+    #     value=["2023-06-01", "2023-06-03"],
+    #     step=0.01,
+    # )
+
+    # date_selector = st.sidebar.date_input(
+    #     "Start date",
+    #     datetime.date(2023, 1, 1),
+    #     datetime.date(2024, 1, 1),
+    # )
+    # st.write(date_selector)
 
     if ranking_mode == "Emissions-Adjusted Cost":
         co_cost = st.slider(
-            "Carbon cost (£/tCO2)", min_value=0.0, max_value=1.0, value=0.5, step=0.01
+            "Carbon cost (£/tCO2)", min_value=0.0, max_value=50.0, value=25.0, step=1.0
         )
 
     # TODO: Replace with function calls
 
-    line_plot = plot_emissions_over_time(cost_ranked_df)
+    line_plot = plot_emissions_over_time(adjusted_costs_df)
     st.plotly_chart(line_plot)
 
 
